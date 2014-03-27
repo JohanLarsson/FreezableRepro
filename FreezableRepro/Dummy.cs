a3323bfc3efd363f4f2bafcf762bbecef953f8d9
unknown <ds2346@SECPC15054.win.dom.sandvik.com>
2014-03-27 19:56:59 +0100

    No repro

---
+++ FreezableRepro/Dummy.cs
@@ -0,0 +1,34 @@
+﻿namespace FreezableRepro
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using System.Windows;
+
+    public class Dummy : Freezable
+    {
+        public static readonly DependencyProperty WidthProperty = DependencyProperty.Register(
+            "Width",
+            typeof (double),
+            typeof (Dummy),
+            new PropertyMetadata(default(double)));
+
+        public double Width
+        {
+            get
+            {
+                return (double) GetValue(WidthProperty);
+            }
+            set
+            {
+                SetValue(WidthProperty, value);
+            }
+        }
+        protected override Freezable CreateInstanceCore()
+        {
+            return new Dummy();
+        }
+    }
+}
---
+++ FreezableRepro/DummyCollection.cs
@@ -0,0 +1,14 @@
+﻿namespace FreezableRepro
+{
+    using System.Collections.ObjectModel;
+    using System.Windows;
+
+    public class DummyCollection : UIElement
+    {
+        public DummyCollection()
+        {
+            Dummies = new ObservableCollection<Dummy>();
+        }
+        public ObservableCollection<Dummy> Dummies { get; set; }
+    }
+}
